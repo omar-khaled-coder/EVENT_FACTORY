f09a828d3c7bbc1ba189ba3331470649e9ff13f0
omarabdrbuo <omarkh3010@gmail.com>
2024-07-08 18:31:17 +0800

    finshed profile picture

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,10 @@ class PagesController < ApplicationController
   def user_profile
     @user = current_user
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :phone_number, :profile_picture, :govt_id_picture)
+  end
 end
