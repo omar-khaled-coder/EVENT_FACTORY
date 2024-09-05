4a504dca65c3d35d14db0ff4ff2308d6195a7c17
omarabdrbuo <omarkh3010@gmail.com>
2024-09-05 17:37:31 +0800

    100%

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/user_profile", to: "pages#user_profile"
+  get "/user_profile/:id", to: "pages#user_profile", as: "user_profile"
   get "/space_requests", to: "pages#space_requests"
   get 'policies', to: 'pages#policies'
 
