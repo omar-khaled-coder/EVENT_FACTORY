07e89a4e462750b4445cc095f1804238ac5c9740
omarabdrbuo <omarkh3010@gmail.com>
2024-09-07 20:37:44 +0800

    done

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -4,4 +4,26 @@ class Chatroom < ApplicationRecord
   belongs_to :space
 
   has_many :messages, dependent: :destroy
+
+  # Check if there are new messages for the current user
+  def has_new_messages?(current_user)
+    last_message = messages.order(created_at: :desc).first
+    return false unless last_message
+
+    # Check if the user is the owner or the user and compare the last viewed time
+    if current_user == owner
+      owner_last_viewed_at.nil? || last_message.created_at > owner_last_viewed_at
+    else
+      user_last_viewed_at.nil? || last_message.created_at > user_last_viewed_at
+    end
+  end
+
+  # Count the number of new messages since the last time the user or owner viewed the chatroom
+  def new_message_count(current_user)
+    if current_user == owner
+      messages.where("created_at > ?", owner_last_viewed_at || Time.zone.now).count
+    else
+      messages.where("created_at > ?", user_last_viewed_at || Time.zone.now).count
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_05_101734) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_07_113317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_05_101734) do
     t.bigint "space_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "owner_last_viewed_at"
+    t.datetime "user_last_viewed_at"
     t.index ["owner_id"], name: "index_chatrooms_on_owner_id"
     t.index ["space_id"], name: "index_chatrooms_on_space_id"
     t.index ["user_id"], name: "index_chatrooms_on_user_id"
