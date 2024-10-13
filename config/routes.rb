ee6247cc13279006c6603e8298b5fa145530ffbd
omarabdrbuo <omarkh3010@gmail.com>
2024-10-14 02:56:56 +0800

    done

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   end
   resources :spaces
   devise_for :users
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
