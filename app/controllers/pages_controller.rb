0463df9503c66b3fd6a796dcdeb851fdac6131dc
omarabdrbuo <omarkh3010@gmail.com>
2024-10-15 02:13:08 +0800

    test done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:user_profile]
+  before_action :authorize_space_requests_access, only: [:space_requests]
+
   def home
     start_date = params[:start_date] ? Date.parse(params[:start_date]) : Date.today
     start_date_range = start_date.beginning_of_month..start_date.end_of_month
@@ -35,6 +38,12 @@ class PagesController < ApplicationController
   end
 
   private
+
+  def authorize_space_requests_access
+    unless user_signed_in? && current_user.email == "omar@gmail.com"
+      redirect_to root_path, alert: "You are not authorized to access this page."
+    end
+  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :phone_number, :profile_picture, :govt_id_picture)
